a5ecebde6948359db04390286b0671d623aee3f7
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2007-06-23 20:05:48 +0000

    Prepare version 0.06.   Bump copyright dates as appropriate.

--- lib/Wx/GLCanvas.pm
+++ lib/Wx/GLCanvas.pm
@@ -1,11 +1,11 @@
 #############################################################################
-## Name:        GLCanvas.pm
+## Name:        lib/Wx/GLCanvas.pm
 ## Purpose:     loader for Wx::GLCanvas.pm
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     26/07/2003
 ## RCS-ID:      $Id$
-## Copyright:   (c) 2003, 2005 Mattia Barbon
+## Copyright:   (c) 2003, 2005, 2007 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
 #############################################################################
@@ -16,7 +16,7 @@ use strict;
 use Wx;
 use base 'Wx::ScrolledWindow';
 
-$Wx::GLCanvas::VERSION = '0.05';
+$Wx::GLCanvas::VERSION = '0.06';
 
 Wx::load_dll( 'gl' );
 Wx::wx_boot( 'Wx::GLCanvas', $Wx::GLCanvas::VERSION );
